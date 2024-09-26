b4b973a3e7d1f6a7c006bdc0e13946f20d406bb3
IT24102799 <it24102799@my.sliit.lk>
2024-09-26 19:06:59 +0530

    Add files via upload

---
+++ Q2/IT24102799Lab7Q2C.java
@@ -0,0 +1,17 @@
+import java .util.Scanner;
+
+
+
+public class IT24102799Lab7Q2C{
+public static void main(String[] args){
+  int rows = 5;
+  
+  for(int i = rows; i >= 1; i--){
+	  for (int j = 1; j <= i; j++){
+		  System.out.print(i);
+	  }
+	  System.out.println();
+  }
+
+}
+}
